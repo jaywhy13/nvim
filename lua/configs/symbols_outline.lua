e3b840482d3c6f4d4238c38af9fe5ab262d106c9
Jean-Mark Wright <jmwright@MAC-778.local>
2024-11-13 09:48:38 -0500

    Configuration of symbols outline

--- lua/configs/symbols_outline.lua
+++ lua/configs/symbols_outline.lua
@@ -1,1 +1,9 @@
-require("symbols-outline").setup()
+local opts = {
+  autofold_depth = 2,
+  keymaps = {
+    fold_all = "H",
+    unfold_all = "L",
+  },
+}
+
+require("symbols-outline").setup(opts)
