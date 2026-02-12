64d5967e2e7f4d5dcb493d61d2451ef48fe7aeac
Jean-Mark Wright <jeanmark.wright@gmail.com>
2026-02-12 10:08:29 -0500

    Move some configuration changes for folding with nvim-ufo
    I'm not exactly sure what I was trying to accomplish
    here, but there's a comment about avoiding replacing
    configurations.

--- lua/configs/nvim-ufo.lua
+++ lua/configs/nvim-ufo.lua
@@ -1,16 +1,8 @@
 -- Option 2: nvim lsp as LSP client
 -- Tell the server the capability of foldingRange,
 -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
-}
-local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-for _, ls in ipairs(language_servers) do
-  require("lspconfig")[ls].setup {
-    capabilities = capabilities,
-    -- you can add other fields for setting up lsp server in this table
-  }
-end
+-- I added this configuration directly in lspconfig.lua since
+-- there is already a capabilities variable being set up for lsp servers
+-- I didn't want to risk replacing that setup
+-- UFO seems to work fine regardless
 require("ufo").setup()
