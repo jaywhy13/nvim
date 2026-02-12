102b9bbe5db55d5816f95ceaf4a26e4bda43b5db
Jean-Mark Wright <jeanmark.wright@gmail.com>
2026-02-12 10:05:04 -0500

    Added prettier/terraform formattting.
    Also tweaked settings a little bit.

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -4,14 +4,16 @@ local options = {
     -- Conform will run multiple formatters sequentially
     python = { "isort", "black" },
     -- Use a sub-list to run only the first available formatter
+    json = { "prettierd", "prettier" },
     javascript = { "prettierd", "prettier" },
     typescript = { "prettierd", "prettier" },
     typescriptreact = { "prettierd", "prettier" },
+    terraform = { "terraform_fmt" },
   },
   format_on_save = {
-    timeout_ms = 1200,
-    lsp_fallback = true,
-    stop_after_first = true,
+    timeout_ms = 500,
+    lsp_fallback = "fallback",
+    stop_after_first = false,
   },
   notify_on_error = false,
 }
