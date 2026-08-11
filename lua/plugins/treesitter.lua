d14b9db4dddbbb38a6de90e2d06528fba17c7d65
Jean-Mark Wright <jeanmark.wright@shopify.com>
2026-08-11 13:29:10 -0400

    Merge Treesitter parsers instead of replacing them
    Use list_insert_unique so parsers added by AstroCommunity language packs
    survive, and add the bash parser.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,19 +2,23 @@
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "vimdoc",
-      "html",
-      "css",
-      "python",
-      "typescript",
-      "terraform",
-      "markdown",
-      "markdown_inline",
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	opts = function(_, opts)
+		if opts.ensure_installed == "all" then
+			return
+		end
+
+		opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed or {}, {
+			"lua",
+			"vim",
+			"vimdoc",
+			"html",
+			"css",
+			"bash",
+			"python",
+			"terraform",
+			"markdown",
+			"markdown_inline",
+		})
+	end,
 }
